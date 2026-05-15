3876e164029916bf56112484e60334389174ab8a
Tkwong <108210425+tk-wong@users.noreply.github.com>
2026-05-15 19:16:36 +0800

    feat: Update ObjectBox initialization and enhance widget tests for scoreboard functionality

--- lib/models/object_box.dart
+++ lib/models/object_box.dart
@@ -13,12 +13,12 @@ class ObjectBox {
 
   /// Create an instance of ObjectBox to use throughout the app.
   static Future<ObjectBox> create() async {
-    final docsDir = await getApplicationDocumentsDirectory();
     // Future<Store> openStore() {...} is defined in the generated objectbox.g.dart
     late final Store store;
     if (kDebugMode) {
       store = await openStore(directory: "memory:objectbox-score-app");
     } else {
+      final docsDir = await getApplicationDocumentsDirectory();
       store = await openStore(
         directory: p.join(docsDir.path, "objectbox-score-app"),
       );
