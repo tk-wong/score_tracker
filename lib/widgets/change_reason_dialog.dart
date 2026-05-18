66b6f882faa9b95bbaaa519e2eb4d3e24432919f
tk-wong <108210425+tk-wong@users.noreply.github.com>
2026-05-18 15:58:03 +0800

    feat: Enhance ChangeReasonDialog with validation and dynamic UI updates

--- lib/widgets/change_reason_dialog.dart
+++ lib/widgets/change_reason_dialog.dart
@@ -10,21 +10,83 @@ class ChangeReasonDialog extends StatefulWidget {
 }
 
 enum ScoreChangeType { increase, decrease }
+
 class _ChangeReasonDialogState extends State<ChangeReasonDialog> {
   final TextEditingController _customReasonController = TextEditingController();
   final TextEditingController _customDeltaController = TextEditingController();
   final List<bool> _selectedWeather = <bool>[false, false, true];
-  final List<(ScoreChangeType, Icon)> icons = <(ScoreChangeType, Icon)> [
+  final List<(ScoreChangeType, Icon)> icons = <(ScoreChangeType, Icon)>[
     (ScoreChangeType.increase, Icon(Icons.add)),
     (ScoreChangeType.decrease, Icon(Icons.remove)),
   ];
-  Set<ScoreChangeType> selectedIcons = <ScoreChangeType>{ScoreChangeType.increase};
+  Set<ScoreChangeType> selectedIcons = <ScoreChangeType>{
+    ScoreChangeType.increase,
+  };
   ScoreReason? _selectedDefault;
+  bool _isEmptyWhenSubmit = false;
+  FocusNode _focus = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _onFocusChange();
+  }
+
+  void _onFocusChange() {
+    _focus.addListener(() {
+      setState(() {}); // Trigger rebuild to update the UI based on focus change
+    });
+  }
+
   @override
   void dispose() {
     _customReasonController.dispose();
     _customDeltaController.dispose();
+    _focus.removeListener(_onFocusChange);
+    _focus.dispose();
     super.dispose();
+  }
+
+  void _onSubmitForm() async {
+    final int customDelta = _customDeltaController.text.trim().isEmpty
+        ? 1
+        : int.parse(_customDeltaController.text.trim());
+    final String customReason = _customReasonController.text.trim();
+    if (_selectedDefault == null && customReason.isEmpty) {
+      setState(() {
+        _isEmptyWhenSubmit = true;
+      });
+      await showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Error'),
+            content: const Text('Please enter a default reason.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+
+    if (_selectedDefault != null) {
+      Navigator.of(context).pop(_selectedDefault);
+      return;
+    }
+
+    Navigator.of(context).pop(
+      ScoreReason(
+        label: customReason,
+        delta: selectedIcons.first == ScoreChangeType.increase
+            ? customDelta
+            : -customDelta,
+      ),
+    );
   }
 
   @override
@@ -39,34 +101,47 @@ class _ChangeReasonDialogState extends State<ChangeReasonDialog> {
             // const SizedBox(height: 8),
             TextField(
               controller: _customReasonController,
+              focusNode: _focus,
               maxLength: 60,
               decoration: InputDecoration(
                 labelText: 'New reason',
                 hintText: 'Example: "Won a game"',
+                labelStyle: TextStyle(
+                  color: _isEmptyWhenSubmit && !_focus.hasFocus
+                      ? Colors.red
+                      : Colors.grey,
+                ), // Switches color dynamically
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: _isEmptyWhenSubmit
+                        ? Colors.red
+                        : Colors.grey, // Switches color dynamically
+                  ),
+                ),
               ),
               onChanged: (String value) {
-                if (_selectedDefault != null) {
+                if (value.trim().isNotEmpty) {
                   setState(() {
-                    _selectedDefault = null;
+                    _isEmptyWhenSubmit =
+                        false; // Reset error state when user types
                   });
                 }
               },
             ),
             SegmentedButton<ScoreChangeType>(
               segments: icons
-                  .map(
-                    (icon) =>
-                        ButtonSegment(value: icon.$1, icon: icon.$2),
-                  )
+                  .map((icon) => ButtonSegment(value: icon.$1, icon: icon.$2))
                   .toList(),
               // direction: vertical ? Axis.vertical : Axis.horizontal,
               onSelectionChanged: (Set<ScoreChangeType> newSelection) {
-              setState(() {
-                selectedIcons = newSelection;
-              });
+                setState(() {
+                  selectedIcons = newSelection;
+                });
               },
               showSelectedIcon: false,
-              selected: selectedIcons.isEmpty ? {ScoreChangeType.increase} : {selectedIcons.first},
+              selected: selectedIcons.isEmpty
+                  ? {ScoreChangeType.increase}
+                  : {selectedIcons.first},
             ),
             TextField(
               controller: _customDeltaController,
@@ -92,7 +167,7 @@ class _ChangeReasonDialogState extends State<ChangeReasonDialog> {
           child: const Text('Cancel'),
         ),
         FilledButton(
-          onPressed: null, // TODO: implement submit logic
+          onPressed: _onSubmitForm,
           child: const Text('Confirm'),
         ),
       ],
